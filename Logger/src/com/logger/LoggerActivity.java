163e99c92afe6e327c1aa243880df37ce5aeb26b
Kajsa Goffrich <kajsa.goffrich@gmail.com>
2013-03-29 21:07:08 +0100

    Display log entries.

--- Logger/src/com/logger/LoggerActivity.java
+++ Logger/src/com/logger/LoggerActivity.java
@@ -1,7 +1,11 @@
 package com.logger;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.*;
 
 public class LoggerActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,15 @@ public class LoggerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        ListView logEntryList = (ListView)findViewById(R.id.logEntryList);
+        logEntryList.setAdapter(new LogEntryAdapter(getApplicationContext(), new LogEntries().getAll()));
+    }
+    
+    class LogEntryAdapter extends ArrayAdapter<LogEntry> {
+
+		public LogEntryAdapter(Context context, List<LogEntry> objects) {
+			super(context, R.layout.log_entry_item, R.id.text, objects);
+		}
+    	
     }
 }
