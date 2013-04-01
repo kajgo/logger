cc206d9928f66e5872820fc28442f5e9e8c93d90
Rickard Lindberg <ricli85@gmail.com>
2013-04-01 15:22:55 +0200

    Change to horizontal layout for items.

--- Logger/gen/com/logger/R.java
+++ Logger/gen/com/logger/R.java
@@ -14,9 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050001;
+        public static final int button1=0x7f050000;
         public static final int logEntryList=0x7f050002;
-        public static final int text=0x7f050000;
+        public static final int text=0x7f050001;
     }
     public static final class layout {
         public static final int log_entry_item=0x7f030000;
