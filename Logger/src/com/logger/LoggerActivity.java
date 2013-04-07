a1db841e0cb8f76bf018055b7784c062ce244d48
Kajsa Goffrich and Rickard Lindberg <kajsa.goffrich+ricli85@gmail.com>
2013-04-07 10:38:29 +0200

    Add log message.

--- Logger/src/com/logger/LoggerActivity.java
+++ Logger/src/com/logger/LoggerActivity.java
@@ -1,5 +1,6 @@
 package com.logger;
 
+import java.util.Date;
 import java.util.List;
 
 import android.app.Activity;
@@ -12,12 +13,26 @@ import android.widget.*;
 
 public class LoggerActivity extends Activity {
 	
+    private LogEntries log = new LogEntries();
+        
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        log.add(new LogEntry("first entry", new Date(100)));
+        log.add(new LogEntry("seond entry", new Date(200)));
+        
         ListView logEntryList = (ListView)findViewById(R.id.logEntryList);
-        logEntryList.setAdapter(new LogEntryAdapter(getApplicationContext(), new LogEntries().getAll()));
+        logEntryList.setAdapter(new LogEntryAdapter(getApplicationContext(), log.getAll()));
+    }
+    
+    public void logButtonClicked(View view) {
+        EditText logMessage = (EditText)findViewById(R.id.logMessageText);
+        log.add(new LogEntry(logMessage.getText().toString(), new Date(100)));
+        
+        ListView logEntryList = (ListView)findViewById(R.id.logEntryList);
+        logEntryList.setAdapter(new LogEntryAdapter(getApplicationContext(), log.getAll()));
     }
     
     class LogEntryAdapter extends ArrayAdapter<LogEntry> {
